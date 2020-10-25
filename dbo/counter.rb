828941f80bc69383aca6326ac2d8dfc8b48a3ae1
Ray Brinzer <ray.brinzer@gmail.com>
2020-10-25 09:25:48 -0400

    Reorganzied project layout, split the Counter class into its own file.

---
+++ dbo/counter.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+
+
+class Counter
+	def initialize
+		reset
+		@t1 = Time.now
+		@last_report = nil
+	end
+	def reset
+		@i = 0
+	end
+	def to_s
+		@i += 1
+		return '.' unless @i % 10 == 0
+		return "%-4i %22s\n" % [ @i, elapsed_time_if_new ]
+	end
+	def elapsed_seconds
+		Time.now - @t1
+	end
+	def elapsed_time
+		format_time(elapsed_seconds)
+	end
+	def elapsed_time_if_new
+		old = @last_report
+		@last_report = new = elapsed_time
+		return '' if old == new
+		format_time(elapsed_seconds)
+	end
+	def format_time t
+		base  = Time.new(0) + t
+		units = [ :day, :hour, :min, :sec ]
+		parts = units.map { |u| [u, base.send(u)] }.to_h
+		parts[:day] -= 1
+		ret = "%i:%02i:%02i:%02i elapsed" % parts.values
+		ret.sub /^0:(?:00:)(?:0)?/, ''
+	end
+end
