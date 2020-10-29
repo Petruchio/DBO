432cfdf3e69acfe53d4c836968b2195e6450a198
Ray Brinzer <ray.brinzer@gmail.com>
2020-10-29 18:59:35 -0400

    Abstracted record printing modes from dbo-template into dbo/reporter.rb

--- dbo/reporter.rb
+++ dbo/reporter.rb
@@ -23,5 +23,45 @@ module DBO
 			'-' * 80 + "\n"
 		end
 
+		def print_records_as_json records
+			records.each do |record|
+				puts record.to_json
+			end
+		end
+
+		def print_records_as_csv records
+			records.each do |r|
+				puts r.values.to_csv
+			end
+		end
+
+		def print_records_as_tsv records
+			records.each do |r|
+				puts r.values * "\t"
+			end
+		end
+
+		def print_values_as_sql records
+			records.each do |r|
+				puts bar
+				puts r.strip
+			end
+		end
+
+		alias print_records print_records_as_tsv
+
+		def output style
+			case style
+			when :json
+				alias print_records print_records_as_json
+			when :csv
+				alias print_records print_records_as_csv
+			when :tsv
+				alias print_records print_records_as_tsv
+			when :sql
+				alias print_records print_values_as_sql
+			end
+		end
+
 	end
 end
